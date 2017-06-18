754ff776a57c19af3de7f6e72613478e1568e154
tamanugi <tamanugi@gmail.com>
2017-06-18 19:31:25 +0900

    initial commit

---
+++ lib/mix_task_deps_add.ex
@@ -0,0 +1,18 @@
+defmodule MixTaskDepsAdd do
+  @moduledoc """
+  Documentation for MixTaskDepsAdd.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MixTaskDepsAdd.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mix_task_deps_add_test.exs
@@ -0,0 +1,8 @@
+defmodule MixTaskDepsAddTest do
+  use ExUnit.Case
+  doctest MixTaskDepsAdd
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
